11349c317c06d17a457400cd379cfe91e7f3a292
robschwartz26 <robbyschwartz@gmail.com>
2026-04-27 14:29:34 -0700

    feat: add Capacitor for iOS native app wrap (com.plaster.the.wall.app)

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.plaster.the.wall.app',
+  appName: 'plaster',
+  webDir: 'dist'
+};
+
+export default config;
